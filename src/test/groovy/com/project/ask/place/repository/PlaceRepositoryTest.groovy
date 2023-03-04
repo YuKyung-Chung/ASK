5dc36ed6cba31306572de0e0a9fb4f493925cb63
YuKyung-Chung <kadie@naver.com>
2023-03-05 00:54:06 +0900

    feat: kakao 주소검색 api 테스트 케이스 작성

--- src/test/groovy/com/project/ask/place/repository/PlaceRepositoryTest.groovy
+++ src/test/groovy/com/project/ask/place/repository/PlaceRepositoryTest.groovy
@@ -12,6 +12,11 @@ class PlaceRepositoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private PlaceRepository placeRepository
 
+    //시작 전 실행되는 메서드
+    def setup() {
+        placeRepository.deleteAll()
+    }
+
     def "PlaceRepository save"(){
         given:
         String placeName = "무릉계곡";
@@ -34,6 +39,27 @@ class PlaceRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getPlaceName() == placeName
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+    def "PlaceRepository saveAll"(){
+        given:
+        String placeName = "무릉계곡";
+        String placeAddress = "강원도 동해시 삼화로 584";
+        double latitude = 37.463551;
+        double longitude = 129.014511;
+
+        def place = Place.builder()
+                .placeAddress(placeAddress)
+                .placeName(placeName)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+        when:
+        placeRepository.saveAll(Arrays.asList(place))
+        def result = placeRepository.findAll()
+
+        then:
+        result.size() == 1
     }
 }
 
